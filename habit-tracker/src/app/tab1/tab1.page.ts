906a1e8098f42ca1902038eae26d2c63aaa74d7b
Brendon Rofe <brofe93@gmail.com>
2026-04-28 15:09:15 +0200

    update: replace *ngIf and such with @if and @for

--- habit-tracker/src/app/tab1/tab1.page.ts
+++ habit-tracker/src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, FormsModule, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, FormsModule, CommonModule],
 })
 export class Tab1Page {
   habits = [
